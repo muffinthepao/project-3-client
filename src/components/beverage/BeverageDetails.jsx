d7c91d60d9cf3df9aeac15d779b006a9b476a433
Mervin Poh <mervpoh@gmail.com>
2022-09-05 21:43:28 +0800

    fix: beverage show route, basic details out. still no CSS

--- src/components/beverage/BeverageDetails.jsx
+++ src/components/beverage/BeverageDetails.jsx
@@ -1,16 +1,39 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import BeverageCard from "../beverage-card/BeverageCard";
+
+import axios from "axios";
+
+const baseURL = "http://localhost:8000/api/v1/beverages";
 
 function BeverageDetails(props) {
-    const params = useParams()
+    const params = useParams();
+    const [beverage, setBeverage] = useState(null);
+
+    useEffect(() => {
+        axios
+            .get(`${baseURL}/${params.beverageId}`)
+            .then((response) => {
+                setBeverage(response.data);
+            })
+        
+        // if (!beverage) return null;
+    }, []);
+
+    console.log("beverage: ", beverage);
 
     return (
         <>
-            <h1>particular Beverage Card</h1>
-            <h4>{params.beverageId}</h4>
+            <h4>Beverage ID: {params.beverageId}</h4>
+            <img src={beverage.img} />
+            <h1>Name: {beverage.name}</h1>
+            <p>Brand: {beverage.brandName}</p>
+            <p>Price: {beverage.price}</p>
+            <p>Spec: {beverage.spec}</p>
+            <p>Stock: {beverage.stock}</p>
+            <p>Description: {beverage.description}</p>
+
         </>
-    )
+    );
 }
 
-export default BeverageDetails
+export default BeverageDetails;
