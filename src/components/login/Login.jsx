98c67f8456a940206bba4ea7e0467d4a9b797de8
Mervin Poh <mervpoh@gmail.com>
2022-09-09 16:48:14 +0800

    fix: login/reggo form scss

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-// import Button from 'react-bootstrap/Button';
 import { Link } from "react-router-dom";
-import ImageComponent from "../image-component/ImageComponent";
-import loginImg from "../login/refreshing_beverage_td3r.svg";
-
 import { useForm } from "react-hook-form";
 import { joiResolver } from "@hookform/resolvers/joi";
 import Joi from "joi";
+
+import styles from '../stylesheets/form.module.scss'
+import ImageComponent from "../image-component/ImageComponent";
+import loginImg from "../login/refreshing_beverage_td3r.svg";
+
 
 const schema = Joi.object({
     email: Joi.string()
@@ -38,39 +39,44 @@ function Login(props) {
 
     return (
         <>
-            <div className="container">
+            <div className="container my-5">
                 <div className="row">
-                    <div className="col-8 border border-primary">
-                        <img src={ImageComponent(loginImg)} alt="login.img" />
+
+                    <div className="col-7">
+                        <img  src={ImageComponent(loginImg)} alt="login.img" />
                     </div>
-                    <div className="col-sm-4 border border-danger">
-                        <form onSubmit={handleSubmit(onSubmit)}>
-                            <div className="form-group">
-                                <label for="email">Email address</label>
-                                <input
-                                    type="email"
-                                    className="form-control"
-                                    id="email"
-                                    {...register("email")}
-                                    placeholder="email"
-                                />
-                                <p>{errors.email?.message}</p>
-                            </div>
-                            <div className="form-group">
-                                <label for="password">Password</label>
-                                <input
-                                    type="password"
-                                    className="form-control"
-                                    id="password"
-                                    {...register("password")}
-                                    placeholder="Password"
-                                />
-                                <p>{errors.password?.message}</p>
-                            </div>
-                            <button type="submit" className="btn btn-primary">
-                                Login
-                            </button>
-                        </form>
+
+                    <div className="col-sm-5 rounded-4 border bg-light">
+                        <div className="p-3">
+                            <h5 className="mb-4 d-flex justify-right">REGISTER</h5>
+                            <form onSubmit={handleSubmit(onSubmit)}>
+                                <div className={styles["form-group"]}>
+                                    <label className={styles['form-label']} for="email">Email Address</label>
+                                    <input
+                                        type="email"
+                                        className="form-control"
+                                        id="email"
+                                        {...register("email")}
+                                        placeholder="email"
+                                    />
+                                    <p className={styles['form-error-message']}>{errors.email?.message}</p>
+                                </div>
+                                <div className={styles["form-group"]}>
+                                    <label className={styles['form-label']} for="password">Password</label>
+                                    <input
+                                        type="password"
+                                        className="form-control"
+                                        id="password"
+                                        {...register("password")}
+                                        placeholder="Password"
+                                    />
+                                    <p className={styles['form-error-message']}>{errors.password?.message}</p>
+                                </div>
+                                <div class="d-grid gap-2">
+                                    <button class="btn btn-primary" type="submit">Button</button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -6,6 +6,7 @@ import { joiResolver } from "@hookform/resolvers/joi";
 import Joi from "joi"; 
 
 // import validators from "../validators/formValidators"
+import styles from '../stylesheets/form.module.scss'
 import ImageComponent from "../image-component/ImageComponent";
 import RegisterImg from "../register/refreshing_ncum.svg";
 
@@ -46,76 +47,77 @@ function Register(props) {
 
     return (
         <>
-            <div className="container">
+            <div className="container my-5">
                 <div className="row">
-                    <div className="col-sm-8 border border-primary">
-                        <img
-                            src={ImageComponent(RegisterImg)}
-                            alt="register.img"
-                        />
+                    <div className="col-sm-7">
+                        <img id={styles["register-image"]} src={ImageComponent(RegisterImg)} alt="register.img"/>
                     </div>
-                    <div className="col-sm-4 border border-danger">
-                        <form onSubmit={handleSubmit(onSubmit)}>
-                            <div className="form-group">
-                                <label for="fullName" className="d-flex justify-left">Full Name</label>
-                                <input
-                                    type="text"
-                                    className="form-control"
-                                    id="fullName"
-                                    {...register("fullName")}
-                                    placeholder="John Doe"
-                                />
-                                <p>{errors.fullName?.message}</p>
-                            </div>
-                            <div className="form-group">
-                                <label for="preferredName">Preferred Name</label>
-                                <input
-                                    type="text"
-                                    className="form-control"
-                                    id="preferredName"
-                                    {...register("preferredName")}
-                                    placeholder="John"
-                                />
-                                <p>{errors.preferredName?.message}</p>
-                            </div>
-                            <div className="form-group">
-                                <label for="email">Email</label>
-                                <input
-                                    type="email"
-                                    className="form-control"
-                                    id="email"
-                                    {...register("email")}
-                                    placeholder="abc@email.com"
-                                />
-                                <p>{errors.email?.message}</p>
-                            </div>
-                            <div className="form-group">
-                                <label for="password">Password</label>
-                                <input
-                                    type="password"
-                                    className="form-control"
-                                    id="password"
-                                    {...register("password")}
-                                    placeholder="********"
-                                    default="123"
-                                />
-                                <p>{errors.password?.message}</p>
-                            </div>
-                            <div className="form-group">
-                                <label for="confirmPassword">Confirm Password</label>
-                                <input
-                                    type="password"
-                                    className="form-control"
-                                    id="confirmPassword"
-                                    {...register("confirmPassword")}
-                                    placeholder="********"
-                                />
-                                <p>{errors.confirmPassword?.message}</p>
-                            </div>
-                            <button type="submit" className="btn btn-primary">
-                                Register
-                            </button>
-                        </form>
+
+                    <div className="col-sm-5 rounded-4 border bg-light bg-sm-none">
+                        <div className="p-4">
+                            <h5 className="mb-4 d-flex justify-right">REGISTER</h5>
+                            <form onSubmit={handleSubmit(onSubmit)}>
+                                <div className={styles["form-group"]}>
+                                    <label for="fullName" className={styles['form-label']}>Full Name</label>
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        id="fullName"
+                                        {...register("fullName")}
+                                        placeholder="John Doe"
+                                    />
+                                    <p className={styles['form-error-message']}>{errors.fullName?.message}</p>
+                                </div>
+                                <div className={styles["form-group"]}>
+                                    <label className={styles['form-label']} for="preferredName">Preferred Name</label>
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        id="preferredName"
+                                        {...register("preferredName")}
+                                        placeholder="John"
+                                    />
+                                    <p className={styles['form-error-message']}>{errors.preferredName?.message}</p>
+                                </div>
+                                <div className={styles["form-group"]}>
+                                    <label className={styles['form-label']} for="email">Email</label>
+                                    <input
+                                        type="email"
+                                        className="form-control"
+                                        id="email"
+                                        {...register("email")}
+                                        placeholder="abc@email.com"
+                                    />
+                                    <p className={styles['form-error-message']}>{errors.email?.message}</p>
+                                </div>
+                                <div className={styles["form-group"]}>
+                                    <label className={styles['form-label']} for="password">Password</label>
+                                    <input
+                                        type="password"
+                                        className="form-control"
+                                        id="password"
+                                        {...register("password")}
+                                        placeholder="********"
+                                        default="123"
+                                    />
+                                    <p className={styles['form-error-message']}>{errors.password?.message}</p>
+                                </div>
+                                <div className={styles["form-group"]}>
+                                    <label className={styles['form-label']} for="confirmPassword">Confirm Password</label>
+                                    <input
+                                        type="password"
+                                        className="form-control"
+                                        id="confirmPassword"
+                                        {...register("confirmPassword")}
+                                        placeholder="********"
+                                    />
+                                    <p className={styles['form-error-message']}>{errors.confirmPassword?.message}</p>
+                                </div>
+                                <div class="d-grid gap-2">
+                                    <button class="btn btn-primary" type="submit">Register</button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
