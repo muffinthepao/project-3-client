8de3f74b98751310c69227202c8a762bb4e7438a
James Teo Gee Hoe <jamesteogh@gmail.com>
2022-09-13 22:46:29 +0800

    fix:price to 2decimal, card component image linked to show page

--- src/components/beverage-card/BeverageCard.jsx
+++ src/components/beverage-card/BeverageCard.jsx
@@ -20,10 +20,10 @@ function BeverageCard(props) {
                     {props.data ? (
                         
                             <Card style={{ width: "18rem", height: "600px" }}>
-                                <Card.Img className={styles['img']} variant="top" src={ImageComponent(img)} />
                                 <Link to={`/beverages/${_id}`}>
+                                <Card.Img className={styles['img']} variant="top" src={ImageComponent(img)} />
                                     <Card.Body>
-                                            <Card.Text>${price}</Card.Text>
+                                            <Card.Text>${price.toFixed(2)}</Card.Text>
                                             {/* svg for the add favourite heart */}
                                             <svg width="24" height="24" viewBox="0 0 24 24" color="#0d6efd"><title>favourite</title><path d="M20.5 9.225a4.73 4.73 0 0 1-1.38 3.34l-6.766 6.788a.5.5 0 0 1-.708 0l-6.767-6.788a4.734 4.734 0 0 1 0-6.68 4.701 4.701 0 0 1 6.664 0l.457.458.457-.458a4.703 4.703 0 0 1 6.663 0 4.73 4.73 0 0 1 1.38 3.34zM12 18.292l5.6-5.62.812-.813a3.73 3.73 0 0 0 0-5.268 3.703 3.703 0 0 0-5.247 0l-.811.814a.5.5 0 0 1-.708 0l-.812-.814a3.701 3.701 0 0 0-5.247 0 3.734 3.734 0 0 0 0 5.268L12 18.292z" fill="currentColor" fill-rule="evenodd"></path></svg>
                                             <Card.Title>{name}</Card.Title>
