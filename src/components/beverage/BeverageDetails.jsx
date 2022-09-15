8c14d6460d74c3670dbc66441776e01f0fe0b9ef
James Teo Gee Hoe <jamesteogh@gmail.com>
2022-09-15 22:19:20 +0800

    feat: install react bootstrap show page manage to put picture and data side by each other fixed image problem

--- src/components/beverage/BeverageDetails.jsx
+++ src/components/beverage/BeverageDetails.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Button } from "react-bootstrap";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import styles from './beverage-details.scss';
 
 import axios from "axios";
@@ -58,30 +61,36 @@ function BeverageDetails(props) {
 
     return beverage ? (
         <>
-            <div className={styles['maincard']}>
-                {/* <h4>Beverage ID: {beverageId}</h4> */}
-            <div className={styles['seccard']}>
-                <img src={ImageComponent(beverage.img)} alt={beverage.name} />
-            </div>
-            <div className={styles['seccard']}>
-                <h4>Name: {beverage.name}</h4>
-                <p>Brand: {beverage.brandName}</p>
-                <p>Price: <b>${beverage.price.toFixed(2)}</b></p>
-                <p>Spec: {beverage.spec}</p>
-                <p>Stock: {beverage.stock}</p>
-                <p>Description: {beverage.description}</p>
-                <div className={styles['addCart']}>
-                    {/* svg for increse and decrease icon together with input field */}
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40" data-testid="SvgRemoveFromCart" height="40" width="40"><title></title><g fill="none"><circle cx="20" cy="20" r="20" fill="#0d6efd"></circle><rect width="2" height="16" x="19" y="12" fill="#FFF" rx="1" transform="rotate(90 20 20)"></rect></g></svg>
-                    <input aria-label="quantity" name="quantity" type="number" autocomplete="off" min="0" value="1" pattern="\d*" className={styles['inputOrder']}></input>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40" data-testid="SvgAddToCart" height="40" width="40"><title></title><g fill="none"><circle cx="20" cy="20" r="20" fill="#0d6efd"></circle><g fill="#FFF" transform="translate(12 12)"><rect width="2" height="16" x="7" rx="1" transform="rotate(90 8 8)"></rect><rect width="2" height="16" x="7" rx="1"></rect></g></g></svg>
-                    <Button variant="primary" className="addcartbutton">Add to Cart</Button>
-                </div>
-            </div>
-            </div>
+            <Container>
+                <Row>
+                    <Col>
+                            <img src={ImageComponent(beverage.img)} alt={beverage.name} />
+                    </Col>
+                    <Col>
+                        <h4>${beverage.price.toFixed(2)}</h4>
+                        <p>{beverage.name}</p>
+                        <p>Brand: {beverage.brandName}</p>
+                        <p>{beverage.spec}</p>
+                        <p>Stock: {beverage.stock}</p>
+                        
+                        <div className={styles['addCart']}>
+                            {/* svg for increse and decrease icon together with input field */}
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40" data-testid="SvgRemoveFromCart" height="40" width="40"><title></title><g fill="none"><circle cx="20" cy="20" r="20" fill="#0d6efd"></circle><rect width="2" height="16" x="19" y="12" fill="#FFF" rx="1" transform="rotate(90 20 20)"></rect></g></svg>
+                            <input aria-label="quantity" name="quantity" type="number" autocomplete="off" min="0" value="1" pattern="\d*" className={styles['inputOrderShow']}></input>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40" data-testid="SvgAddToCart" height="40" width="40"><title></title><g fill="none"><circle cx="20" cy="20" r="20" fill="#0d6efd"></circle><g fill="#FFF" transform="translate(12 12)"><rect width="2" height="16" x="7" rx="1" transform="rotate(90 8 8)"></rect><rect width="2" height="16" x="7" rx="1"></rect></g></g></svg>
+                            <Button variant="primary" className="addcartbutton">Add to Cart</Button>
+                        </div>
+                    </Col>
+                </Row>
+                <Row>
+                    <h3>Description</h3>
+                    <p>{beverage.description}</p>
+                </Row>
+            </Container>
         </>
     ) : null
 }
 
 export default BeverageDetails;
+
 
